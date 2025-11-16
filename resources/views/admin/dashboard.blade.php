e1906cfe9fc1d4f67094d6b403e0cf93762dec6a
DarlynZen <145638485+DarlynZen@users.noreply.github.com>
2025-11-16 14:42:18 -0500

    Cambiar estructura admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,5 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        @include('admin.partials.sidebar')
     </x-slot>
 </x-admin-layout>
--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -7,30 +7,30 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Panel de Administración') }}</title>
+
     <link rel="icon" type="image/png" href="{{ asset('assets/images/logo-uns-rojo.png') }}" />
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-neutral-400">
+<body class="font-sans antialiased bg-neutral-200">
 
-        <livewire:layout.navigation />
-        <header class="bg-white shadow">
-            {{ $header }}
-        </header>
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+    {{-- <livewire:layout.navigation /> --}}
+
+    <div class="h-full flex flex-col w-full">
+        <div>
+            <header class="bg-white shadow p-4">
+                {{ $header ?? '' }}
+            </header>
+        </div>
+        <div>
+            @include('admin.partials.sidebar')
+        </div>
     </div>
-
-
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 
 </html>
